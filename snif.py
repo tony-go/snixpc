840971c064061e2f3975260eb77e010fd6f5559f
Tony Gorez <gorez.tony@gmail.com>
2024-07-29 18:32:23 +0200

    wip
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- snif.py
+++ snif.py
@@ -5,6 +5,61 @@ def __lldb_init_module(debugger, internal_dict):
     debugger.HandleCommand('command script add -f snif.set_xpc_breakpoints snif')
     print("XPC Tracker plugin loaded. Use 'snif' to set breakpoints on XPC functions.")
 
+def execute_command(command):
+    interpreter = lldb.debugger.GetCommandInterpreter()
+    result = lldb.SBCommandReturnObject()
+    interpreter.HandleCommand(command, result)
+    if result.Succeeded():
+        return result.GetOutput().strip()
+    else:
+        return result.GetError()
+
+def serialize_xpc_dictionary(frame, xpc_dict):
+    objc_code = f'''
+    @import Foundation;
+
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+
+    [dict setObject:@"value" forKey:@"key"];
+
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:&error];
+    if (error) {{
+        NSLog(@"Error serializing JSON: %@", error);
+    }}
+    
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    [jsonString cString];
+    '''
+
+    result = frame.EvaluateExpression(objc_code)
+    
+    # Ensure the JSON string is properly formatted
+    value = result.GetSummary().strip('"')
+    value = value.replace('\\n', '').replace('\\', '')
+
+    # Parse the JSON string
+    try:
+        json_obj = json.loads(value)
+        return json_obj
+    except json.JSONDecodeError as e:
+        return {error: "Failed to parse JSON"}
+
+def serialize_xpc_message(frame, xpc_obj):
+    # Get Type address
+    type_addr = frame.EvaluateExpression(f'(const char*) xpc_get_type((void*) {xpc_obj})').GetValue()
+
+    if not type_addr:
+        return {"type": "Unknown", "description": "Failed to get type address"}
+
+    # Use po to get the type name
+    type_name = execute_command(f'po {type_addr}')
+
+    if "OS_xpc_dictionary" in type_name:
+        return serialize_xpc_dictionary(frame, xpc_obj)
+    else:
+        return {"type": type_name, "description": "Unknown type"}
+
 def send_callback(frame, bp_loc, internal_dict):
     function_name = frame.GetFunctionName()
     
@@ -19,8 +74,7 @@ def send_callback(frame, bp_loc, internal_dict):
     xpc_connection_get_pid_expr = f'(int)xpc_connection_get_pid((void *){conn})'
     conn_pid = frame.EvaluateExpression(xpc_connection_get_pid_expr).GetValue()
 
-    # Get message
-    message = frame.EvaluateExpression(f'(const char*) xpc_copy_description((void*) {msg})').GetSummary()
+    message = serialize_xpc_message(frame, msg)
 
     # print json object with all details
     xpc_data = {
@@ -33,7 +87,7 @@ def send_callback(frame, bp_loc, internal_dict):
 
     print(json.dumps(xpc_data, indent=4))
     
-    return False
+    return True
 
 def recv_callback(frame, bp_loc, internal_dict):
     function_name = frame.GetFunctionName()
@@ -77,8 +131,8 @@ def set_xpc_breakpoints(debugger, command, result, internal_dict):
     for func in xpc_send_functions:
         breakpoint = target.BreakpointCreateByName(func)
         breakpoint.SetScriptCallbackFunction('snif.send_callback')
-        breakpoint.SetOneShot(False)
-        breakpoint.SetAutoContinue(True) 
+        # breakpoint.SetOneShot(False)
+        # breakpoint.SetAutoContinue(True) 
         print(f"Set breakpoint on: {func}")
 
 
