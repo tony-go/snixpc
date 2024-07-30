20536e50d6dbc9ad908cee1dc2fe0be95c76f677
Tony Gorez <gorez.tony@gmail.com>
2024-07-30 13:35:52 +0200

    fix: proper serialization for xpc messages
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- snif.py
+++ snif.py
@@ -15,132 +15,75 @@ def execute_command(command):
         return result.GetError()
 
 def serialize_xpc_dictionary(frame, xpc_dict):
-    # objc_code = f'''
-    # @import Foundation;
-    #
-    # NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    #
-    # [dict setObject:@"value" forKey:@"key"];
-    #
-    # NSError *error = nil;
-    # NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dict options:NSJSONWritingPrettyPrinted error:&error];
-    # if (error) {{
-    #     NSLog(@"Error serializing JSON: %@", error);
-    # }}
-    # 
-    # NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    # [jsonString cString];
-    # '''
     
     objc_code = f'''
     @import Foundation;
 
-    // Define necessary XPC types and functions
     typedef void* xpc_object_t;
-    typedef void* xpc_type_t;
-
+    typedef const struct _xpc_type_s * xpc_type_t;
+    
     extern xpc_type_t xpc_get_type(xpc_object_t object);
-    extern const char* xpc_string_get_string_ptr(xpc_object_t xstring);
+    extern const char *xpc_string_get_string_ptr(xpc_object_t xstring);
     extern int64_t xpc_int64_get_value(xpc_object_t xint);
     extern uint64_t xpc_uint64_get_value(xpc_object_t xuint);
     extern double xpc_double_get_value(xpc_object_t xdouble);
     extern bool xpc_bool_get_value(xpc_object_t xbool);
-    extern const void* xpc_data_get_bytes_ptr(xpc_object_t xdata);
+    extern const void *xpc_data_get_bytes_ptr(xpc_object_t xdata);
     extern size_t xpc_data_get_length(xpc_object_t xdata);
-    extern const char* xpc_copy_description(xpc_object_t object);
-
     extern bool xpc_dictionary_apply(xpc_object_t xdict, bool (^applier)(const char *key, xpc_object_t value));
-    extern bool xpc_array_apply(xpc_object_t xarray, bool (^applier)(size_t index, xpc_object_t value));
+    
+    extern const struct _xpc_type_s _xpc_type_string;
+    extern const struct _xpc_type_s _xpc_type_int64;
+    extern const struct _xpc_type_s _xpc_type_uint64;
+    extern const struct _xpc_type_s _xpc_type_bool;
+    extern const struct _xpc_type_s _xpc_type_double;
+    extern const struct _xpc_type_s _xpc_type_data;
+    
+    #define XPC_TYPE_STRING (&_xpc_type_string)
+    #define XPC_TYPE_INT64 (&_xpc_type_int64)
+    #define XPC_TYPE_UINT64 (&_xpc_type_uint64)
+    #define XPC_TYPE_BOOL (&_xpc_type_bool)
+    #define XPC_TYPE_DOUBLE (&_xpc_type_double)
+    #define XPC_TYPE_DATA (&_xpc_type_data)
+    
+    NSMutableDictionary *result = [NSMutableDictionary dictionary];
+    xpc_dictionary_apply((xpc_object_t){xpc_dict}, ^bool(const char *key, xpc_object_t value) {{
+        NSString *keyStr = [NSString stringWithCString:key encoding:NSUTF8StringEncoding];
+        xpc_type_t type = xpc_get_type(value);
+        
+        if (type == XPC_TYPE_STRING) {{
+            result[keyStr] = [NSString stringWithCString:xpc_string_get_string_ptr(value) encoding:NSUTF8StringEncoding];
+        }} else if (type == XPC_TYPE_INT64) {{
+            result[keyStr] = @(xpc_int64_get_value(value));
+        }} else if (type == XPC_TYPE_UINT64) {{
+            result[keyStr] = @(xpc_uint64_get_value(value));
+        }} else if (type == XPC_TYPE_BOOL) {{
+            result[keyStr] = @(xpc_bool_get_value(value));
+        }} else if (type == XPC_TYPE_DOUBLE) {{
+            result[keyStr] = @(xpc_double_get_value(value));
+        }} else if (type == XPC_TYPE_DATA) {{
+            NSData *data = [NSData dataWithBytes:xpc_data_get_bytes_ptr(value) length:xpc_data_get_length(value)];
+            result[keyStr] = [data base64EncodedStringWithOptions:0];
+        }} else {{
+            result[keyStr] = @"Unknown type";
+        }}
+        
+        return true;
+    }});
 
-    // Define XPC types as integer constants (these values are made up and may not match the actual values)
-    #define XPC_TYPE_DICTIONARY 1
-    #define XPC_TYPE_ARRAY 2
-    #define XPC_TYPE_STRING 3
-    #define XPC_TYPE_INT64 4
-    #define XPC_TYPE_UINT64 5
-    #define XPC_TYPE_DOUBLE 6
-    #define XPC_TYPE_BOOL 7
-    #define XPC_TYPE_DATA 8
-
-    NSString *result = (^NSString *() {{
-        __block NSString *(^serializeXPCObject)(void *);
-
-        NSString *(^serializeXPCDictionary)(void *) = ^NSString *(void *dict) {{
-            NSMutableDictionary *result = [NSMutableDictionary dictionary];
-            xpc_dictionary_apply(dict, ^bool(const char *key, void *value) {{
-                NSString *keyStr = [NSString stringWithCString:key encoding:NSUTF8StringEncoding];
-                result[keyStr] = serializeXPCObject(value);
-                return true;
-            }});
-            NSError *error = nil;
-            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:0 error:&error];
-            if (error) {{
-                return @"{{}}";
-            }}
-            return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        }};
-
-        NSString *(^serializeXPCArray)(void *) = ^NSString *(void *array) {{
-            NSMutableArray *result = [NSMutableArray array];
-            xpc_array_apply(array, ^bool(size_t index, void *value) {{
-                [result addObject:serializeXPCObject(value)];
-                return true;
-            }});
-            NSError *error = nil;
-            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:0 error:&error];
-            if (error) {{
-                return @"[]";
-            }}
-            return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-        }};
-
-        serializeXPCObject = ^NSString *(void *obj) {{
-            xpc_type_t type = xpc_get_type(obj);
-            if (type == XPC_TYPE_DICTIONARY) {{
-                return serializeXPCDictionary(obj);
-            }} else if (type == XPC_TYPE_ARRAY) {{
-                return serializeXPCArray(obj);
-            }} else if (type == XPC_TYPE_STRING) {{
-                return [NSString stringWithCString:xpc_string_get_string_ptr(obj) encoding:NSUTF8StringEncoding];
-            }} else if (type == XPC_TYPE_INT64) {{
-                return [NSString stringWithFormat:@"%lld", xpc_int64_get_value(obj)];
-            }} else if (type == XPC_TYPE_UINT64) {{
-                return [NSString stringWithFormat:@"%llu", xpc_uint64_get_value(obj)];
-            }} else if (type == XPC_TYPE_DOUBLE) {{
-                return [NSString stringWithFormat:@"%f", xpc_double_get_value(obj)];
-            }} else if (type == XPC_TYPE_BOOL) {{
-                return xpc_bool_get_value(obj) ? @"true" : @"false";
-            }} else if (type == XPC_TYPE_DATA) {{
-                NSData *data = [NSData dataWithBytes:xpc_data_get_bytes_ptr(obj) length:xpc_data_get_length(obj)];
-                return [data base64EncodedStringWithOptions:0];
-            }} else {{
-                return [NSString stringWithCString:xpc_copy_description(obj) encoding:NSUTF8StringEncoding];
-            }}
-        }};
-
-        void *xpc_dict = (void *){xpc_dict};
-        return serializeXPCObject(xpc_dict);
-    }})();
-
-    result;
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:result options:NSJSONWritingPrettyPrinted error:&error];
+    error ? [error localizedDescription] : [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding]
     '''
 
-    res = execute_command(f'expression -l objc -O -- {objc_code}')
-    print(f"Result: {res}")
+    result = execute_command(f'expression -l objc -O -- {objc_code}')
 
-    return "foo"
-
-
-    # Ensure the JSON string is properly formatted
-    # value = result.GetSummary().strip('"')
-    # value = value.replace('\\n', '').replace('\\', '')
-    #
-    # # Parse the JSON string
-    # try:
-    #     json_obj = json.loads(value)
-    #     return json_obj
-    # except json.JSONDecodeError as e:
-    #     return {error: "Failed to parse JSON"}
+    # Parse the JSON string
+    try:
+        json_obj = json.loads(result)
+        return json_obj
+    except json.JSONDecodeError as e:
+        return {error: "Failed to parse JSON"}
 
 def serialize_xpc_message(frame, xpc_obj):
     # Get Type address
